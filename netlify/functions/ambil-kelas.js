c6ec2b53582d51b10cd464c7e3fb990eb09e6927
Dicky Miswardi <dicky_miswardi@yahoo.com>
2025-08-08 13:47:46 +0700

    Update ambil-kelas.js

--- netlify/functions/ambil-kelas.js
+++ netlify/functions/ambil-kelas.js
@@ -1,30 +1,23 @@
 const fetch = require('node-fetch');
-const token = process.env.MTQ_TOKEN;
 
-exports.handler = async () => {
-  const url = `https://api.github.com/repos/dickymiswardi/usermtq/contents`;
+exports.handler = async function () {
+  const token = process.env.MTQ_TOKEN;
+  const folderUrl = `https://api.github.com/repos/dickymiswardi/usermtq/contents`;
 
-  try {
-    const res = await fetch(url, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-        Accept: 'application/vnd.github.v3+json'
-      }
-    });
+  const res = await fetch(folderUrl, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      Accept: 'application/vnd.github.v3+json'
+    }
+  });
 
-    const files = await res.json();
-    const kelasFiles = files
-      .filter(file => file.name.startsWith("kelas_") && file.name.endsWith(".json"))
-      .map(file => file.name.replace(".json", ""));
+  const files = await res.json();
+  const kelas = files
+    .filter(f => f.name.startsWith('kelas_') && f.name.endsWith('.json'))
+    .map(f => f.name.replace('.json', ''));
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify(kelasFiles)
-    };
-  } catch (err) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: err.message })
-    };
-  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify(kelas)
+  };
 };
