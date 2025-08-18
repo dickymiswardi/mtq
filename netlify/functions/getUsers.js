dff78cd4cefee6606fdc42c92a2f6b28a555cc91
Dicky Miswardi <dicky_miswardi@yahoo.com>
2025-08-18 19:04:39 +0700

    Update getUsers.js

--- netlify/functions/getUsers.js
+++ netlify/functions/getUsers.js
@@ -1,27 +1,29 @@
+import { Buffer } from "node:buffer";
+
 export async function handler() {
-  const token = process.env.MTQ_TOKEN;
-  const githubApiUrl = "https://api.github.com/repos/dickymiswardi/usermtq/contents/user.json";
+  const token = process.env.MTQ2_TOKEN;
+  const githubApiUrl = "https://api.github.com/repos/digitalmtq/back/contents/users.json";
 
   try {
     const response = await fetch(githubApiUrl, {
       headers: {
-        Authorization: `Bearer ${token}`,
+        Authorization: `token ${token}`,
         Accept: "application/vnd.github.v3+json"
       }
     });
 
     if (!response.ok) {
-      throw new Error(`Gagal fetch data: ${response.status}`);
+      throw new Error(`Gagal fetch data: ${response.status} ${response.statusText}`);
     }
 
     const result = await response.json();
 
     // Decode base64 ke teks
-    const content = Buffer.from(result.content, 'base64').toString('utf-8');
+    const content = Buffer.from(result.content, "base64").toString("utf-8");
 
     return {
       statusCode: 200,
-      body: content
+      body: content // langsung JSON string
     };
   } catch (error) {
     return {
